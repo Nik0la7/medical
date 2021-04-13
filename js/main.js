66c30cdb4f6886bc5151127acdf17d36ba1e78e2
Nik0la7 <74868186+Nik0la7@users.noreply.github.com>
2021-04-13 21:42:37 +0400

    Website without mobile version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-    $('.slider-doc').slick();
+    $('.slider-doc').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        autoplay: true,
+        speed: 3000,
+      });
 
 
 
