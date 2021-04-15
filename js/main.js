35c62649fa438bd3232192ce4a26856a3407efa1
Nik0la7 <74868186+Nik0la7@users.noreply.github.com>
2021-04-15 16:13:11 +0400

    Medical
    Medical

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('.slider-doc').slick({
         infinite: true,
         slidesToShow: 3,
@@ -7,9 +6,14 @@ $(function(){
         dots: true,
         autoplay: true,
         speed: 3000,
-      });
-
-
-
-
+      });  
 });
+$('.open-burger').on('click', function(e) {
+  e.preventDefault;
+  $(this).toggleClass('burger-active'); //бургер крутится
+  $('openMenu').css('display', 'inline-block');// тут бля не пойму как его сделать нужно что б при нажатии кнопки паралельно бургеру отобразилась скрытая менюха
+  $(this).toggleClass('openMneu-active')
+  
+  
+});
+
